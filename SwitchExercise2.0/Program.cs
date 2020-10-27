38f580a323b7d96d40c33b06f602a9f5afd644a6
Aaron <aaron.johnson35043@gmail.com>
2020-10-27 14:31:56 -0500

    Add project files.

---
+++ SwitchExercise2.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SwitchExercise2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 8;
+
+            Console.WriteLine("Guess the favorite number. Hint: It is under 10");
+            var userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine($"That number is too low!");
+            }
+            else if (userGuess > favNumber)
+            {
+                Console.WriteLine($"That number is too high!");
+            }
+            else if (userGuess == favNumber)
+            {
+                Console.WriteLine($"Correct!!!");
+            }
+            else
+            {
+                Console.WriteLine($"Nevermind!");
+            }
+            
+
+            
+
+        
+
+        }
+    }
+}
